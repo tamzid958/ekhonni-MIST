9402c0bc4c134aff50efd916058ee636fb0391a0
Mayome <tahsina.mayome0703@gmail.com>
2024-02-19 16:48:47 +0600

    Add post review feature for Admin

---
+++ backend/src/main/java/com/dsi/backend/service/AdminService.java
@@ -0,0 +1,13 @@
+package com.dsi.backend.service;
+
+import com.dsi.backend.model.Product;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public interface AdminService {
+
+//    ResponseEntity<?> loginAdmin(String email, String password);
+}
--- backend/src/main/java/com/dsi/backend/service/ProductService.java
+++ backend/src/main/java/com/dsi/backend/service/ProductService.java
@@ -6,8 +6,13 @@ import com.dsi.backend.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public interface ProductService {
     Product saveProduct(Product product);
 
+    Product updateProduct(Long id, Boolean isApprovedByAdmin);
+
+    List<Product> fetchAllProducts();
 }
