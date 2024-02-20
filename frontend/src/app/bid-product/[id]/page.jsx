b9baa4c64d43557fe113b372bc25c1c1754c4674
Shadman Shafeen Khan <shadmanskystar@gmail.com>
2024-02-20 16:10:08 +0600

    bugfix/change routes and fix login

--- frontend/src/app/bid-product/[id]/page.jsx
+++ frontend/src/app/bid-product/[id]/page.jsx
@@ -2,58 +2,61 @@
 
 import Image from "next/image";
 import axios from "axios";
-import {useEffect} from "react";
+import {useEffect, useState} from "react";
+import Button from "@/components/Button";
 
 
 const ProductBidPage = ({ params }) => {
 
+    const [data , setData] = useState([]);
     const product_id = params.id;
-    const  fetchProductDetails = async () => {
-        const response = await axios.get(`http://localhost:8080/api/v1/products/${product_id}`);
-        console.log(response);
-    }
-    useEffect(() => {
-        fetchProductDetails().then(r => {
-            console.log(r);
-        });
-    }, []);
+    // const  fetchProductDetails = async () => {
+    //     const response = await axios.get(`http://localhost:8080/api/v1/products/${product_id}`);
+    //     console.log(response);
+    //     setData(response.data);
+    // }
+    // useEffect(() => {
+    //     fetchProductDetails().then(r => {
+    //         console.log(r);
+    //     });
+    // }, []);
+
+
     return (
         <div className="w-screen h-[700px] flex justify-center items-center">
             <div className="w-4/5 h-5/6 border-2 border-black flex flex-row justify-start items-center rounded-lg">
-                <div className="w-1/2 h-full border-2 border-red-600 flex justify-center items-center">
-                    <div className="w-[95%] h-[90%] border-2 border-green-500 relative">
-                        <Image src={"/dslr.jpg"} alt={"dslr"} priority fill style={{borderRadius:"3%"}}/>
+                <div className="w-1/2 h-full  flex justify-center items-center">
+                    <div className="w-[95%] h-[95%]  relative">
+                        <Image src={"/dslr.jpg"} alt={"dslr"} fill objectFit={"cover"} style={{borderRadius:"3%"}}/>
                     </div>
                 </div>
-                <div className="w-1/2 h-full border-2 border-blue-500 flex justify-center items-center">
-                    <div className="w-3/4 h-full border-2 border-amber-300 flex flex-col justify-center items-center">
-                        <div className="w-full h-1/5 flex justify-start items-center  border-2 border-black">
+                <div className="w-1/2 h-full flex justify-center items-center">
+                    <div className="w-3/4 h-full flex flex-col justify-center items-center">
+                        <div className="w-full h-1/5 flex justify-start items-center  border-b">
                             <h1 className="text-3xl text-black font-medium text-left">
                                 Product Name
                             </h1>
                         </div>
-                        <div className="w-full h-1/5 flex flex-row border-2 border-black">
+                        <div className="w-full h-1/5 flex flex-col justify-start items-center border-b">
                             <div className="w-full h-1/3 flex">
-                                <p className="text-lg">Category , </p>
-                                <p className="text-lg ml-2">Subcategory</p>
+                                <p className="text-base">Category , Subcategory</p>
                             </div>
                             <div className="w-full h-1/3 flex">
-                                <p className="text-lg"></p>
+                                <p className="text-base">Location</p>
                             </div>
                             <div className="w-full h-1/3 flex">
-                                <p className="text-lg"></p>
+                                <p className="text-base">For sale by sellerName</p>
                             </div>
                         </div>
-                        <div className="w-full h-1/5 border-2 border-black">
-
+                        <div className="w-full h-1/5 flex items-center border-b">
+                            <p className="text-lg">Description</p>
                         </div>
-                        <div className="w-full h-1/5 border-2 border-black">
-
+                        <div className="w-full h-1/5 flex items-center border-b">
+                            <p className="text-xl">Current Price</p>
                         </div>
-                        <div className="w-full h-1/5 border-2 border-black">
-
+                        <div className="w-full h-1/5 flex justify-center items-center border-b">
+                            <Button value={"Bid"} option={1} type={"button"} />
                         </div>
-
                     </div>
                 </div>
             </div>
