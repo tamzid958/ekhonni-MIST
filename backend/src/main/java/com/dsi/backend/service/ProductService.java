4217db7a0d94bda27af47e057f8461f84454cf1c
Mayome <tahsina.mayome0703@gmail.com>
2024-03-05 11:55:57 +0600

    Add fetch Category list with subcategories

--- backend/src/main/java/com/dsi/backend/service/ProductService.java
+++ backend/src/main/java/com/dsi/backend/service/ProductService.java
@@ -20,11 +20,7 @@ public interface ProductService {
 
     List<ProductView> fetchAllRequests();
 
-//    List<ProductView> fetchAllProducts();
-
     ProductView getProductById(Long id);
-
-//    Page<ProductView> fetchProducts(int page, FilterRequest filter);
     Map<String,Long> countProducts(String division);
     List<ProductView> showByCategory(String category);
     Category insertCategory(Category category);
